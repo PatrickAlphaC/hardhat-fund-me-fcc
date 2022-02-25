2eecfe09fd3c2ac01f2ef63c97923333c21a6759
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2022-02-24 23:15:37 -0500

    updated for front-end

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -18,4 +18,7 @@ export const networkConfig: networkConfigInfo = {
   },
 }
 
+export const frontEndContractsFile =
+  "../nextjs-smartcontract-lottery-fcc/constants/contractAddresses.json"
+
 export const developmentChains = ["hardhat", "localhost"]
