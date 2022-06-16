4c0f6b5c9c1e088368cc595e2e915982289601a1
gmluqa <gmluqa@protonmail.com>
2022-06-16 20:18:37 +0300

    Input params for rinkeby test deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,6 +14,9 @@ const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 const KOVAN_RPC_URL =
     process.env.KOVAN_RPC_URL ||
     "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
+const RINKEBY_RPC_URL =
+    process.env.RINKEBY_RPC_URL ||
+    "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
 const PRIVATE_KEY =
     process.env.PRIVATE_KEY ||
     "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
@@ -31,6 +34,11 @@ module.exports = {
             accounts: [PRIVATE_KEY],
             chainId: 42,
             blockConfirmations: 6,
+        },
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
         },
     },
     solidity: {
