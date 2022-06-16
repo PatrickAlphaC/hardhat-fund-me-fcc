c04977ba0bd69e82f0a52f427520602caa0fdef7
gmluqa <gmluqa@protonmail.com>
2022-06-16 21:26:57 +0300

    Upped ether amount (rip), forced gas limit

--- test/staging/FundMe.staging.test.js
+++ test/staging/FundMe.staging.test.js
@@ -7,7 +7,7 @@ developmentChains.includes(network.name)
     : describe("FundMe Staging Tests", async function () {
           let deployer
           let fundMe
-          const sendValue = ethers.utils.parseEther("0.03")
+          const sendValue = ethers.utils.parseEther("0.1")
           beforeEach(async () => {
               deployer = (await getNamedAccounts()).deployer
               fundMe = await ethers.getContract("FundMe", deployer)
@@ -15,9 +15,16 @@ developmentChains.includes(network.name)
 
           it("allows people to fund and withdraw", async function () {
               await fundMe.fund({ value: sendValue })
-              await fundMe.withdraw()
+              await fundMe.withdraw({
+                  gasLimit: 100000,
+              })
+
               const endingFundMeBalance = await fundMe.provider.getBalance(
                   fundMe.address
+              )
+              console.log(
+                  endingFundMeBalance.toString() +
+                      " should equal 0, running assert equal..."
               )
               assert.equal(endingFundMeBalance.toString(), "0")
           })
