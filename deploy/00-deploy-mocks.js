a5f1bf8512b5ab3e95389a9a13dae68d72c89606
Iosif <iosifv@users.noreply.github.com>
2022-06-25 20:56:58 +0100

    Update 00-deploy-mocks.js
    fix comment typo

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -6,7 +6,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
-    // If we are on a local develohelppment network, we need to deploy mocks!
+    // If we are on a local development network, we need to deploy mocks!
     if (chainId == 31337) {
         log("Local network detected! Deploying mocks...")
         await deploy("MockV3Aggregator", {
