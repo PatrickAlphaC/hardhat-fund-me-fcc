01c4bb4e3abbd85cbb071b03bb88c419cdf460f0
MozharAlhosni <109945717+MozharAlhosni@users.noreply.github.com>
2022-08-08 18:24:43 +0800

    Update "revertedWith" to "revertedWithCustomError"
    The latest Hardhat Chai Matchers plugin provides the new matcher "revertedWithCustomError" in case a custom error is used in the contract. The test will no longer pass if "revertedWith" is used with a string while in the contract a custom error is used, as hardhat will output the following error:"AssertionError: Expected transaction to be reverted with reason 'FundMe__NotOwner', but it reverted with a custom error".

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -141,7 +141,7 @@ const { developmentChains } = require("../../helper-hardhat-config")
                   )
                   await expect(
                       fundMeConnectedContract.withdraw()
-                  ).to.be.revertedWith("FundMe__NotOwner")
+                  ).to.be.revertedWithCustomError(fundMe, "FundMe__NotOwner")
               })
           })
       })
