af0c3c243e8d36d6eb5d9aaa24a4494f7fc88bf3
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2022-09-15 20:51:05 -0400

    updated for goerli

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,11 +11,8 @@ require("hardhat-deploy")
  */
 
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
-const KOVAN_RPC_URL =
-    process.env.KOVAN_RPC_URL ||
-    "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
-const RINKEBY_RPC_URL =
-    process.env.RINKEBY_RPC_URL ||
+const GOERLI_RPC_URL =
+    process.env.GOERLI_RPC_URL ||
     "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
 const PRIVATE_KEY =
     process.env.PRIVATE_KEY ||
@@ -29,17 +26,10 @@ module.exports = {
             chainId: 31337,
             // gasPrice: 130000000000,
         },
-        kovan: {
-            url: KOVAN_RPC_URL,
+        goerli: {
+            url: GOERLI_RPC_URL,
             accounts: [PRIVATE_KEY],
-            chainId: 42,
-            blockConfirmations: 6,
-            gas: 6000000,
-        },
-        rinkeby: {
-            url: RINKEBY_RPC_URL,
-            accounts: [PRIVATE_KEY],
-            chainId: 4,
+            chainId: 5,
             blockConfirmations: 6,
         },
     },
