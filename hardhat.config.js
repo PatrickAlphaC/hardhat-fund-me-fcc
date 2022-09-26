2f5b0e0d3ecef386614f9a26edd1d7b231216733
M.Siddique <108999811+ABossOfMyself@users.noreply.github.com>
2022-09-26 20:50:49 +0500

    Update hardhat.config.js
    Changed solidity compiler version 0.8.8 to 0.8.7

--- hardhat.config.js
+++ hardhat.config.js
@@ -36,7 +36,7 @@ module.exports = {
     solidity: {
         compilers: [
             {
-                version: "0.8.8",
+                version: "0.8.7",
             },
             {
                 version: "0.6.6",
